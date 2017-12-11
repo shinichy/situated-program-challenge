0c24ba3ac8d37426babe281e05820ab6994cdb08
Shinichi Katayama <shin1.katayama@gmail.com>
2017-12-11 10:02:56 +0900

    Add more checks in a test

--- test/GroupControllerSpec.scala
+++ test/GroupControllerSpec.scala
@@ -10,6 +10,7 @@ class GroupControllerSpec extends PlaySpec
   with BeforeAndAfterEach
   with JdbcContext
   with Util {
+
   import ctx._
 
   override def beforeEach() {
@@ -25,7 +26,7 @@ class GroupControllerSpec extends PlaySpec
       val lastName1 = "Katayama"
       val email1 = "s@test.com"
       val id1 = createMember(firstName1, lastName1, email1)
-      
+
       val firstName2 = "Kenji"
       val lastName2 = "Nakamura"
       val email2 = "k@test.com"
@@ -45,13 +46,13 @@ class GroupControllerSpec extends PlaySpec
       val responseJson = contentAsJson(result)
       (responseJson \ "group-id").as[Int] must be > 0
       (responseJson \ "group-name").as[String] mustEqual groupName
-      val admin1 = (responseJson \ "admin")(0)
+      val admin1 = (responseJson \ "admin") (0)
       (admin1 \ "member-id").as[Int] mustEqual id1
       (admin1 \ "first-name").as[String] mustEqual firstName1
       (admin1 \ "last-name").as[String] mustEqual lastName1
       (admin1 \ "email").as[String] mustEqual email1
-      
-      val admin2 = (responseJson \ "admin")(1)
+
+      val admin2 = (responseJson \ "admin") (1)
       (admin2 \ "member-id").as[Int] mustEqual id2
       (admin2 \ "first-name").as[String] mustEqual firstName2
       (admin2 \ "last-name").as[String] mustEqual lastName2
@@ -61,19 +62,53 @@ class GroupControllerSpec extends PlaySpec
 
   "POST /members/{member-id}/groups/{group-id}" should {
     "return OK" in {
-      val memberId = createMember("Shinichi", "Katayama", "s@test.com")
-      val memberId2 = createMember("Kenji", "Nakamura", "k@test.com")
-      val groupId = createGroup("clj-nakano", memberId)
+      val firstName1 = "Shinichi"
+      val lastName1 = "Katayama"
+      val email1 = "s@test.com"
+      val memberId1 = createMember(firstName1, lastName1, email1)
 
+      val firstName2 = "Kenji"
+      val lastName2 = "Nakamura"
+      val email2 = "k@test.com"
+      val memberId2 = createMember(firstName2, lastName2, email2)
+
+      val groupName = "clj-nakano"
+      val groupId = createGroup(groupName)
+
+      // join a group as admin
       val json = Json.parse(
         s"""
-          |{
-          |  "admin": true
-          |}
+           |{
+           |  "admin": true
+           |}
         """.stripMargin)
-      val Some(result) = route(app, FakeRequest(POST, s"/members/$memberId2/groups/$groupId").withJsonBody(json))
+      val Some(result) = route(app, FakeRequest(POST, s"/members/$memberId1/groups/$groupId").withJsonBody(json))
 
       status(result) mustEqual OK
+      val responseJson = contentAsJson(result)
+      val admin = (responseJson \ "admin") (0)
+      (admin \ "member-id").as[Int] mustEqual memberId1
+      (admin \ "first-name").as[String] mustEqual firstName1
+      (admin \ "last-name").as[String] mustEqual lastName1
+      (admin \ "email").as[String] mustEqual email1
+
+      // join a group as a member
+      val json2 = Json.parse(
+        s"""
+           |{
+           |  "admin": false
+           |}
+        """.stripMargin)
+      val Some(result2) = route(app, FakeRequest(POST, s"/members/$memberId2/groups/$groupId").withJsonBody(json2))
+
+      status(result2) mustEqual OK
+      val responseJson2 = contentAsJson(result2)
+      println(responseJson2)
+      val member = (responseJson2 \ "members") (0)
+      (member \ "member-id").as[Int] mustEqual memberId2
+      (member \ "first-name").as[String] mustEqual firstName2
+      (member \ "last-name").as[String] mustEqual lastName2
+      (member \ "email").as[String] mustEqual email2
     }
   }
 
@@ -101,34 +136,36 @@ class GroupControllerSpec extends PlaySpec
       val meetupId = createMeetup(groupId1, title, "2017-12-04T07:31:34.248Z", "2017-12-04T09:31:34.248Z", venueId)
 
       val Some(result) = route(app, FakeRequest(GET, "/groups"))
+      status(result) mustEqual OK
       val responseJson = contentAsJson(result)
       val group1 = responseJson(0)
       (group1 \ "group-id").as[Int] mustEqual groupId1
       (group1 \ "group-name").as[String] mustEqual groupName1
-      val admin1 = (group1 \ "admin")(0)
+
+      val admin1 = (group1 \ "admin") (0)
       (admin1 \ "member-id").as[Int] mustEqual id1
       (admin1 \ "first-name").as[String] mustEqual firstName1
       (admin1 \ "last-name").as[String] mustEqual lastName1
       (admin1 \ "email").as[String] mustEqual email1
 
-      val member1 = (group1 \ "members")(0)
+      val venue1 = (group1 \ "venues") (0)
+      (venue1 \ "venue-id").as[Int] mustEqual venueId
+      (venue1 \ "venue-name").as[String] mustEqual venueName
+
+      val meetup1 = (group1 \ "meetups") (0)
+      (meetup1 \ "event-id").as[Int] mustEqual meetupId
+      (meetup1 \ "title").as[String] mustEqual title
+
+      val member1 = (group1 \ "members") (0)
       (member1 \ "member-id").as[Int] mustEqual id2
       (member1 \ "first-name").as[String] mustEqual firstName2
       (member1 \ "last-name").as[String] mustEqual lastName2
       (member1 \ "email").as[String] mustEqual email2
 
-      val venue1 = (group1 \ "venues")(0)
-      (venue1 \ "venue-id").as[Int] mustEqual venueId
-      (venue1 \ "venue-name").as[String] mustEqual venueName
-
-      val meetup1 = (group1 \ "meetups")(0)
-      (meetup1 \ "event-id").as[Int] mustEqual meetupId
-      (meetup1 \ "title").as[String] mustEqual title
-
       val group2 = responseJson(1)
       (group2 \ "group-id").as[Int] mustEqual groupId2
       (group2 \ "group-name").as[String] mustEqual groupName2
-      val admin2 = (group2 \ "admin")(0)
+      val admin2 = (group2 \ "admin") (0)
       (admin2 \ "member-id").as[Int] mustEqual id2
       (admin2 \ "first-name").as[String] mustEqual firstName2
       (admin2 \ "last-name").as[String] mustEqual lastName2
